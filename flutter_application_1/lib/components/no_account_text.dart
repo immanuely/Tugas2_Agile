0032f3998e46add8629dc4a813c3cf06c0a458d1
Immanuel Lumban Toruan <immanuelsihombing91@gmai.com>
2023-07-08 16:01:29 +0700

    Menambah No Account text

---
+++ flutter_application_1/lib/components/no_account_text.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/sign_up/sign_up_screen.dart';
+
+import 'package:flutter_application_1/constants.dart';
+import 'package:flutter_application_1/size_config.dart';
+
+class NoAccountText extends StatelessWidget {
+  const NoAccountText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Belum punya akun? ",
+          style: TextStyle(fontSize: getProportionateScreenWidth(16)),
+        ),
+        GestureDetector(
+          onTap: () => Navigator.pushNamed(context, SignUpScreen.routeName),
+          child: Text(
+            "Sign Up",
+            style: TextStyle(
+                fontSize: getProportionateScreenWidth(16),
+                color: kPrimaryColor),
+          ),
+        ),
+      ],
+    );
+  }
+}
