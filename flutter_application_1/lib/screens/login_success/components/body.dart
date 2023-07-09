83b962b416769bcb24ec22791cec6b95cc14ce1a
PranaYudisthira <114347486+PranaYudisthira@users.noreply.github.com>
2023-07-09 09:55:14 +0700

    Menambah tampilan jika login berhasil

---
+++ flutter_application_1/lib/screens/login_success/components/body.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/default_button.dart';
+import 'package:flutter_application_1/screens/home/home_screen.dart';
+import 'package:flutter_application_1/size_config.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: SizeConfig.screenHeight * 0.04),
+        Image.asset(
+          "assets/images/success.png",
+          height: SizeConfig.screenHeight * 0.4, //40%
+        ),
+        SizedBox(height: SizeConfig.screenHeight * 0.08),
+        Text(
+          "Login Berhasil",
+          style: TextStyle(
+            fontSize: getProportionateScreenWidth(30),
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        const Spacer(),
+        SizedBox(
+          width: SizeConfig.screenWidth * 0.6,
+          child: DefaultButton(
+            text: "Kembali ke Beranda",
+            press: () {
+              Navigator.pushNamed(context, HomeScreen.routeName);
+            },
+          ),
+        ),
+        const Spacer(),
+      ],
+    );
+  }
+}
