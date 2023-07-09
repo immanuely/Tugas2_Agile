49b1ede54ebbc2bf28a2612ecce25dbaf481dafa
PranaYudisthira <114347486+PranaYudisthira@users.noreply.github.com>
2023-07-09 09:40:17 +0700

    Menambahkan fitur Lupa Password

---
+++ flutter_application_1/lib/screens/forgot_password/components/body.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/custom_surfix_icon.dart';
+import 'package:flutter_application_1/components/default_button.dart';
+import 'package:flutter_application_1/components/form_error.dart';
+import 'package:flutter_application_1/components/no_account_text.dart';
+import 'package:flutter_application_1/screens/lupa/lupa_succes_screen.dart';
+import 'package:flutter_application_1/size_config.dart';
+
+import '../../../constants.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: Column(
+            children: [
+              SizedBox(height: SizeConfig.screenHeight * 0.04),
+              Text(
+                "Lupa Password",
+                style: TextStyle(
+                  fontSize: getProportionateScreenWidth(28),
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "Silakan masukkan email Anda dan kami akan mengirimkan \ntautan untuk kembali ke akun Anda",
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: SizeConfig.screenHeight * 0.1),
+              ForgotPassForm(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ForgotPassForm extends StatefulWidget {
+  @override
+  _ForgotPassFormState createState() => _ForgotPassFormState();
+}
+
+class _ForgotPassFormState extends State<ForgotPassForm> {
+  final _formKey = GlobalKey<FormState>();
+  List<String> errors = [];
+  String? email;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            onSaved: (newValue) => email = newValue,
+            onChanged: (value) {
+              if (value.isNotEmpty && errors.contains(kEmailNullError)) {
+                setState(() {
+                  errors.remove(kEmailNullError);
+                });
+              } else if (emailValidatorRegExp.hasMatch(value) &&
+                  errors.contains(kInvalidEmailError)) {
+                setState(() {
+                  errors.remove(kInvalidEmailError);
+                });
+              }
+              return null;
+            },
+            validator: (value) {
+              if (value!.isEmpty && !errors.contains(kEmailNullError)) {
+                setState(() {
+                  errors.add(kEmailNullError);
+                });
+              } else if (!emailValidatorRegExp.hasMatch(value) &&
+                  !errors.contains(kInvalidEmailError)) {
+                setState(() {
+                  errors.add(kInvalidEmailError);
+                });
+              }
+              return null;
+            },
+            decoration: const InputDecoration(
+              labelText: "Email",
+              hintText: "Masukkan Email Anda",
+              // If  you are using latest version of flutter then lable text and hint text shown like this
+              // if you r using flutter less then 1.20.* then maybe this is not working properly
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Mail.svg"),
+            ),
+          ),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          FormError(errors: errors),
+          SizedBox(height: SizeConfig.screenHeight * 0.1),
+          DefaultButton(
+            text: "Lanjutkan",
+            press: () {
+              if (_formKey.currentState!.validate()) {
+                Navigator.pushNamed(context, LupaSuccessScreen.routeName);
+              }
+            },
+          ),
+          SizedBox(height: SizeConfig.screenHeight * 0.1),
+          NoAccountText(),
+        ],
+      ),
+    );
+  }
+}
